8dc35030ccb7d5c62cfe378fe4ee49139abf0f5b
Venom <felix.irungu@student.moringaschool.com>
2023-02-13 14:38:31 +0300

    Update methods.rb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+# Your code here!
+def greet_programmer 
+    puts "Hello programmer"
+end
+
+greet_programmer
+
+def greet(name ="Felo")
+    puts "Hi there, #{name}"
+end
+
+greet
+
+def greet_with_default(name = "kama")
+    puts "Hello, #{name}"
+end
+
+greet_with_default
+
+def add(num1, num2)
+    puts num1 + num2
+end
+
+sum = add(3, 5)
+
+def halve(number)
+    if number != String
+        return nil
+    end
+    puts number/2
+end
+
+halve(8)
